55ab476b2315e321e7b24615b2f0aa97ab157b4d
Christine Cua <cloudycua@gmail.com>
2018-02-20 19:52:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(speak)
   if speak.upcase != speak
     return "HUH?! SPEAK UP, SONNY!"
-  elseif speak == "I LOVE YOU GRANDMA!"
+  elsif speak == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
