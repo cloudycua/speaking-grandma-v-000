c9e556f54242b26f1dada17b408f9ba3f93b2e0e
Christine Cua <cloudycua@gmail.com>
2018-02-20 19:40:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speak)
-  if speak.upcase == "I LOVE YOU GRANDMA!"
+  if speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif speak.upcase == speak
     puts "NO, NOT SINCE 1938!"
